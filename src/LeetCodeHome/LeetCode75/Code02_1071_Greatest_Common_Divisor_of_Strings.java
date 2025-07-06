b41ae79ad5d5ddd2e6990056345dea3970fdfd7b
burtohee <burtohe@gmail.com>
2025-07-06 02:09:47 -0400

    gcd of string

---
+++ src/LeetCodeHome/LeetCode75/Code02_1071_Greatest_Common_Divisor_of_Strings.java
@@ -0,0 +1,96 @@
+package LeetCodeHome.LeetCode75;
+
+public class Code02_1071_Greatest_Common_Divisor_of_Strings {
+
+
+    public String gcdOfStrings(String str1, String str2) {
+        return processGreatest_Common_Divisor_of_Strings(str1, str2);
+    }
+
+    public static boolean isDivided(String candidate, String str1, String str2)
+    {
+        int candidateLength = candidate.length();
+
+        if(str1.length() % candidateLength != 0 || str2.length() % candidateLength != 0)
+        {
+            return false;
+        }
+
+        int f1 = str1.length() / candidateLength;
+        int f2 = str2.length() / candidateLength;
+
+        return str1.equals(candidate.repeat(f1)) && str2.equals(candidate.repeat(f2));
+    }
+
+    public static String processGreatest_Common_Divisor_of_Strings(String str1, String str2){
+
+//        String minStr = str1.length() < str2.length()? str1: str2;
+
+        int str1Length = str1.length();
+        int str2Length = str2.length();
+
+        int minLength = Math.min(str1Length, str2Length);
+
+
+        for(int i = minLength; i > 0; i--)
+        {
+            String candiate = str1.substring(0, i);
+            if(isDivided(candiate, str1, str2))
+            {
+                return candiate;
+            }
+        }
+        return "";
+    }
+
+    public static void main(String [] args)
+    {
+        String result = processGreatest_Common_Divisor_of_Strings("abc", "abcabc");
+        System.out.println(result);
+    }
+
+
+    class Solution1 {
+        public String gcdOfStrings(String str1, String str2) {
+            if (!(str1 + str2).equals(str2 + str1)) {
+                return "";
+            }
+
+            int lenGCD = gcd(str1.length(), str2.length());
+            return str1.substring(0, lenGCD);
+        }
+
+        private int gcd(int len1, int len2) {
+            while (len2 != 0) {
+                int temp = len1 % len2;
+                len1 = len2;
+                len2 = temp;
+            }
+            return len1;
+        }
+    }
+
+    class Solution2 {
+        public String gcdOfStrings(String str1, String str2) {
+            if (!(str1 + str2).equals(str2 + str1)) {
+                return "";
+            }
+
+            int lenGCD = gcd(str1.length(), str2.length());
+            return str1.substring(0, lenGCD);
+        }
+
+        private int gcd(int len1, int len2) {
+            int minVal = Math.min(len1, len2);
+            for (int i = minVal; i > 0; i--) {
+                if (len1 % i == 0 && len2 % i == 0) {
+                    return i;
+                }
+            }
+            return 1;
+        }
+    }
+
+
+
+}
--- src/_07_class07/Code06_PathSumII.java
+++ src/_07_class07/Code06_PathSumII.java
@@ -102,4 +102,32 @@ public class Code06_PathSumII {
     }
 
 
+    class Solution {
+        static List<List<Integer>> result = new ArrayList();
+        static int [] arr= new int[1000];
+        public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+            result.clear();
+            pathsumhelper( root, 0, targetSum);
+            return result;
+        }
+        static void pathsumhelper(TreeNode root, int  i , int sum){
+            if(root==null) return ;
+            arr[i]=root.val;
+            if(root.left==null && root.right==null){
+                int sum2 =0;
+                for(int j=0; j<=i; j++) sum2= sum2+arr[j];
+                List<Integer> list = new ArrayList();
+                if(sum2==sum){
+                    for(int j=0; j<=i;j++) list.add(arr[j]);
+                    result.add(list);
+                }
+            }
+            pathsumhelper(root.left,i+1,sum);
+            pathsumhelper(root.right,i+1,sum);
+        }
+    }
+
+
 }
+
+
