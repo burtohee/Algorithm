20e6257abfdb0efc2a21690f0bad7923e04229ab
burtohee <burtohe@gmail.com>
2025-07-11 03:58:49 -0400

    merge sort non-recursive

--- src/algorithm_1_starter/_08_class08/Code02_MergeSort.java
+++ src/algorithm_1_starter/_08_class08/Code02_MergeSort.java
@@ -59,41 +59,92 @@ public class Code02_MergeSort {
 
     }
 
+    // method2: non-recursive, main idea is steps.
     public static void mergeSort2(int[] arr) {
+
         if (arr == null || arr.length < 2) {
             return;
         }
+
         int step = 1;
         int N = arr.length;
+
+        // break when step equal or larger than length of array
         while (step < N) {
+
+            // first left group
             int L = 0;
+
+            // there is no left group after N length
             while (L < N) {
+
+//                int M = L + step - 1; // incorrect, because left group might be shorted
+                /*
+                    e.g.
+                        0 1 2 3 | 4 5 6 7 |   8
+                           L1       R1       L2
+                 */
                 int M = 0;
+//                int M = Math.min(N-1, L + step - 1); // incorrect, N-1 is the last index, L + step - 1 is left group is filled with  steps, L + step -1 might overflow, then its result is negative
+
+//                why N - L? we want to know if (N - 1) - L + 1 have how many steps
+
+                // we have left filled with steps
+                // L...M  M+1...R
                 if (N - L >= step) {
                     M = L + step - 1;
-                } else {
+                }
+                // condition when left group can fill steps, so last index will the M
+                // L...N-1
+                else {
                     M = N - 1;
                 }
+
+                // there is no merge, since only 1 left group
+                // L...M
                 if (M == N - 1) {
                     break;
                 }
+
+                // int R = M + 1, incorrect, might overflow again
                 int R = 0;
+
+                // why N - L? we want to know if (N - 1) - (M + 1) + 1 => (N - 1 - M) have how many steps for right group
                 if (N - 1 - M >= step) {
+                    // M+1+step-1....R => M + step
                     R = M + step;
                 } else {
+                    // N - 1
                     R = N - 1;
                 }
+                // L...M   M+1...R
                 merge(arr, L, M, R);
+
+                // check if right group is at the end, if yes, no further action, if no, move the L pointer to R+1
                 if (R == N - 1) {
                     break;
                 } else {
+                    // next L pointer to R + 1 for L pointer
                     L = R + 1;
                 }
             }
+
+            // step to avoid overflow
+            // idea is: int max is 2^31 - 1; so maximum step would be 2^30, here step > N / 2, which does not allow step go over 2^30
             if (step > N / 2) {
                 break;
             }
+            // if only use this, unsafe, might be overflow
             step *= 2;
+
+//            if (step > N / 2) {
+//                break;
+//            }
+//            else
+//            {
+//                step *= 2;
+//            }
+
         }
 
     }
