6290e75ccbcba491198c501ac1b6aeaec05ce7f4
burtohee <burtohe@gmail.com>
2025-07-14 01:16:09 -0400

    increasing triplet subsequence

--- src/LeetCodeHome/LeetCode75/Code10_334_Increasing_Triplet_Subsequence.java
+++ src/LeetCodeHome/LeetCode75/Code10_334_Increasing_Triplet_Subsequence.java
@@ -1,4 +1,42 @@
 package LeetCodeHome.LeetCode75;
 
 public class Code10_334_Increasing_Triplet_Subsequence {
+
+    public boolean increasingTriplet(int[] nums) {
+
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num <= first) {
+                first = num;
+            } else if (num <= second) {
+                second = num;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+   public static void main(String[] args) {
+            Code10_334_Increasing_Triplet_Subsequence code10334IncreasingTripletSubsequence = new Code10_334_Increasing_Triplet_Subsequence();
+
+            int[][] testCases = {
+                    {1, 2, 3, 4, 5},      // true
+                    {5, 4, 3, 2, 1},      // false
+                    {2, 1, 5, 0, 4, 6},   // true
+                    {20, 100, 10, 12, 5, 13}, // true
+                    {1, 1, 1, 1, 1},      // false
+                    {1, 2, 1, 3},          // true
+                    {5, 1, 6}          // false
+            };
+
+            for (int i = 0; i < testCases.length; i++) {
+                boolean result = code10334IncreasingTripletSubsequence.increasingTriplet(testCases[i]);
+                System.out.println("Test case " + (i + 1) + ": " + result);
+            }
+        }
+
+
+
 }
