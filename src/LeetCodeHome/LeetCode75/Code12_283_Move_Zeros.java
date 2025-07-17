d35236dd8d22840aed8f9a8fd023b9854b8b096c
burtohee <burtohe@gmail.com>
2025-07-17 00:18:18 -0400

    leetcode 443 string compression

---
+++ src/LeetCodeHome/LeetCode75/Code12_283_Move_Zeros.java
@@ -0,0 +1,8 @@
+package LeetCodeHome.LeetCode75;
+
+public class Code12_283_Move_Zeros {
+    public void moveZeroes(int[] nums) {
+
+    }
+
+}
---
+++ src/LeetCodeHome/LeetCode75/Code_11_443_String_Compression.java
@@ -0,0 +1,45 @@
+package LeetCodeHome.LeetCode75;
+
+public class Code_11_443_String_Compression {
+
+    public int compress(char[] chars) {
+
+        int write = 0, read = 0, n = chars.length;
+
+        while (read < n) {
+            char current = chars[read];
+            int count = 0;
+
+            // Count consecutive duplicates
+            while (read < n && chars[read] == current) {
+                read++;
+                count++;
+            }
+
+            // Write the character, move the write pointer to next write position
+            chars[write++] = current;
+
+            // If more than one, write count as characters
+            if (count > 1) {
+                String cntStr = String.valueOf(count);
+                for(char c : cntStr.toCharArray())
+                {
+                    chars[write++] = c;
+                }
+            }
+
+
+        }
+
+        return write;
+
+//          i
+//               insert
+//        ["a","a","b","b","c","c","c"]
+
+//                group = 2
+//        ["a","a","b","b","c","c","c"]
+
+    }
+
+}
