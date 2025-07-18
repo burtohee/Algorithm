abf5f35c99d5c732ed5e7588bc6a403039869664
burtohee <burtohe@gmail.com>
2025-07-18 01:48:00 -0400

    Update Code01_1768_Merge_Strings_Alternately.java

--- src/LeetCodeHome/LeetCode75/Code01_1768_Merge_Strings_Alternately.java
+++ src/LeetCodeHome/LeetCode75/Code01_1768_Merge_Strings_Alternately.java
@@ -3,56 +3,47 @@ package LeetCodeHome.LeetCode75;
 public class Code01_1768_Merge_Strings_Alternately {
 
     public String mergeAlternately(String word1, String word2) {
-        return processMergeAlternately(word1, word2);
+        return "";
     }
 
-    public static String processMergeAlternately(String word1, String word2)
-    {
-        StringBuilder stringBuilder = new StringBuilder();
-        int maxLength = Math.max(word1.length(), word2.length());
 
-        for(int i = 0; i < maxLength; i++)
-        {
-            if(i < word1.length())
-            {
-                stringBuilder.append(word1.charAt(i));
-            }
-            if(i < word2.length())
-            {
-                stringBuilder.append(word2.charAt(i));
-            }
-        }
-//        System.out.println("After append: " + stringBuilder);
-        return stringBuilder.toString();
+
+    public static void main(String [] args)
+    {
+        Code01_1768_Merge_Strings_Alternately code011768MergeStringsAlternately = new Code01_1768_Merge_Strings_Alternately();
+        code011768MergeStringsAlternately.mergeAlternately("abc", "pqr");
     }
 
-    public static String processMergeAlternatelyFast(String word1, String word2)
-    {
-        int a = word1.length();
-        int b = word2.length();
-
-        int i=0, j =0;
-
-        StringBuilder stringBuilder = new StringBuilder();
-
-        while(i < a && j <b)
-        {
-            stringBuilder.append(word1.charAt(i++)).append(word2.charAt(j++));
-        }
-        if(i < a)
-        {
-            stringBuilder.append(word1.substring(i));
-        }
-        if(j < b)
-        {
-            stringBuilder.append(word2.substring(j));
+    class Solution1 {
+        public String mergeAlternately(String word1, String word2) {
+            return processMergeAlternately(word1, word2);
         }
 
-        return stringBuilder.toString();
+        public static String processMergeAlternately(String word1, String word2)
+        {
+            int a = word1.length();
+            int b = word2.length();
 
+            int i=0, j =0;
 
+            StringBuilder stringBuilder = new StringBuilder();
 
-//        int a = word1.length();
+            while(i < a && j <b)
+            {
+                stringBuilder.append(word1.charAt(i++)).append(word2.charAt(j++));
+            }
+            if(i < a)
+            {
+                stringBuilder.append(word1.substring(i));
+            }
+            if(j < b)
+            {
+                stringBuilder.append(word2.substring(j));
+            }
+
+            return stringBuilder.toString();
+
+            //        int a = word1.length();
 //        int b = word2.length();
 //        // pointers
 //        int i = 0, j = 0;
@@ -73,20 +64,36 @@ public class Code01_1768_Merge_Strings_Alternately {
 //        }
 
 //        return sb.toString();
+        }
     }
 
-    public static void main(String [] args)
-    {
-        processMergeAlternately("abc", "pqr");
+    class Solution2 {
+        public String mergeAlternately(String word1, String word2) {
+            return processMergeAlternately(word1, word2);
+        }
+
+        public static String processMergeAlternately(String word1, String word2)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            int maxLength = Math.max(word1.length(), word2.length());
+
+            for(int i = 0; i < maxLength; i++)
+            {
+                if(i < word1.length())
+                {
+                    stringBuilder.append(word1.charAt(i));
+                }
+                if(i < word2.length())
+                {
+                    stringBuilder.append(word2.charAt(i));
+                }
+            }
+//        System.out.println("After append: " + stringBuilder);
+            return stringBuilder.toString();
+        }
     }
 
 
 }
 
 
-//class Solution {
-//    public String mergeAlternately(String word1, String word2) {
-//
-//    }
-//}
-//
