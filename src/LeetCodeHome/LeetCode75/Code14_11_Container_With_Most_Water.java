90f395f3bf38d11e2d5f8146b0adb8efba0d5ae5
burtohee <burtohe@gmail.com>
2025-07-20 03:50:14 -0400

    Update Code14_11_Container_With_Most_Water.java

--- src/LeetCodeHome/LeetCode75/Code14_11_Container_With_Most_Water.java
+++ src/LeetCodeHome/LeetCode75/Code14_11_Container_With_Most_Water.java
@@ -8,7 +8,9 @@ public class Code14_11_Container_With_Most_Water {
         int[][] testCases = {
                 {1,8,6,2,5,4,8,3,7}, // 49
                 {1,1}, // 1
-                {1,0} // 0
+                {1,0}, // 0
+                {0,0,0,1,2,0,0,8,0,1}, // 49
+
 //                {6, 7, 1, 2},      // true
 //                {1, 2, 3, 4, 5},      // true
 //                {5, 4, 3, 2, 1},      // false
@@ -30,37 +32,19 @@ public class Code14_11_Container_With_Most_Water {
     }
 
     public int maxArea(int[] height) {
+        int left = 0, right = height.length-1;
+        int low=0, area = Integer.MIN_VALUE;
 
-        int i = 0, j = height.length - 1;
-
-        int max = Integer.MIN_VALUE;
-
-        while(i < j)
+        while(left<right)
         {
+            low = Math.min(height[left], height[right]);
+            area = Math.max(area, low * (right - left));
 
-            int minIndex = height[i] < height[j] ? i : j;
-
-            int area = height[minIndex] * (j - i);
-
-            if(area > max)
-            {
-                max = area;
-            }
-
-            if(minIndex == i)
-            {
-                i = minIndex + 1;
-            }
-            else
-            {
-                j = minIndex - 1;
-            }
-
-
+            while(left<right && height[left] <= low) left++;
+            while(left<right && height[right] <= low) right--;
         }
+        return area;
 
-
-        return max;
     }
 
 
