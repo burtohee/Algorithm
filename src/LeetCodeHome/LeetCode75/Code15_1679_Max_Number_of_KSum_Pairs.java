8f1083d64438c5ed38edf35b139dc9e6c760a265
burtohee <burtohe@gmail.com>
2025-07-20 04:13:34 -0400

    Update Code15_1679_Max_Number_of_KSum_Pairs.java

--- src/LeetCodeHome/LeetCode75/Code15_1679_Max_Number_of_KSum_Pairs.java
+++ src/LeetCodeHome/LeetCode75/Code15_1679_Max_Number_of_KSum_Pairs.java
@@ -1,4 +1,147 @@
 package LeetCodeHome.LeetCode75;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Code15_1679_Max_Number_of_KSum_Pairs {
+
+
+    public static void main(String [] args)
+    {
+        Code15_1679_Max_Number_of_KSum_Pairs code151679MaxNumberOfKSumPairs = new Code15_1679_Max_Number_of_KSum_Pairs();
+        int[][] testCases = {
+                {1,8,6,2,5,4,8,3,7}, // 49
+                {1,1}, // 1
+                {1,0}, // 0
+                {0,0,0,1,2,0,0,8,0,1}, // 49
+
+//                {6, 7, 1, 2},      // true
+//                {1, 2, 3, 4, 5},      // true
+//                {5, 4, 3, 2, 1},      // false
+//                {2, 1, 5, 0, 4, 6},   // true
+//                {20, 100, 10, 12, 5, 13}, // true
+//                {1, 1, 1, 1, 1},      // false
+//                {1, 2, 1, 3},          // true
+//                {5, 1, 6}          // false
+        };
+
+        for (int i = 0; i < testCases.length; i++) {
+            int result = code151679MaxNumberOfKSumPairs.maxOperations(testCases[i]);
+
+            System.out.println("Test case " + (i + 1) + ": " + result);
+        }
+
+
+
+    }
+
+    public int maxOperations(int[] nums, int k) {
+
+        Arrays.sort(nums);
+        int left = 0, right = nums.length - 1;
+
+        int operations = 0;
+        while(left < right)
+        {
+            int sum = nums[left] + nums[right];
+
+            if(sum == k)
+            {
+                operations++;
+                left++;
+                right--;
+            }
+            else if (sum < k)
+            {
+                left++; // Need a larger sum
+            }
+            else
+            {
+                right--; // Need a smaller sum
+            }
+
+        }
+
+
+        return operations;
+    }
+
+
+    class Solution {
+        public int maxOperations(int[] nums, int k) {
+            int n=nums.length,rest,present=0;
+            Arrays.sort(nums);
+            int j=n-1;
+            for(int i=0;i<j;){
+                if(nums[i]+nums[j]==k){
+                    present++;
+                    i++;
+                    j--;
+                }else if((nums[i]+nums[j])>k){
+                    j--;
+                }else if((nums[i]+nums[j])<k){
+                    i++;
+                }
+            }
+            return present;
+        }
+    }
+
+
+    class Solution3 {
+        public int maxOperations(int[] nums, int k) {
+            Map<Integer, Integer> map = new HashMap<>();
+            int operations = 0;
+
+            for (int num : nums) {
+                int complement = k - num;
+
+                if (map.getOrDefault(complement, 0) > 0) {
+                    // Pair found
+                    operations++;
+                    map.put(complement, map.get(complement) - 1);
+                } else {
+                    // Store this number for future pair
+                    map.put(num, map.getOrDefault(num, 0) + 1);
+                }
+            }
+
+            return operations;
+        }
+    }
+
+    class Solution2 {
+        public int maxOperations(int[] nums, int k) {
+
+            Arrays.sort(nums);
+            int left = 0, right = nums.length - 1;
+
+            int operations = 0;
+            while(left < right)
+            {
+                int sum = nums[left] + nums[right];
+
+                if(sum == k)
+                {
+                    operations++;
+                    left++;
+                    right--;
+                }
+                else if (sum < k)
+                {
+                    left++; // Need a larger sum
+                }
+                else
+                {
+                    right--; // Need a smaller sum
+                }
+
+            }
+
+
+            return operations;
+        }
+    }
+
 }
