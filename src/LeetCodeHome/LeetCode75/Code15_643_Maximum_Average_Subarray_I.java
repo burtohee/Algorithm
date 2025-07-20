053b76d65866b665e79ea926347cdb36bf0b9e2a
burtohee <burtohe@gmail.com>
2025-07-20 03:51:05 -0400

    Update Code16_643_Maximum_Average_Subarray_I.java

--- src/LeetCodeHome/LeetCode75/Code15_643_Maximum_Average_Subarray_I.java
+++
@@ -1,4 +0,0 @@
-package LeetCodeHome.LeetCode75;
-
-public class Code15_643_Maximum_Average_Subarray_I {
-}
---
+++ src/LeetCodeHome/LeetCode75/Code16_643_Maximum_Average_Subarray_I.java
@@ -0,0 +1,4 @@
+package LeetCodeHome.LeetCode75;
+
+public class Code16_643_Maximum_Average_Subarray_I {
+}
