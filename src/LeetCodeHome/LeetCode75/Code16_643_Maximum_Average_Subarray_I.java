37ed9393bef933861844af08d60ada4365333711
burtohee <burtohe@gmail.com>
2025-07-20 04:16:33 -0400

    Update Code16_643_Maximum_Average_Subarray_I.java

--- src/LeetCodeHome/LeetCode75/Code16_643_Maximum_Average_Subarray_I.java
+++ src/LeetCodeHome/LeetCode75/Code16_643_Maximum_Average_Subarray_I.java
@@ -1,4 +1,31 @@
 package LeetCodeHome.LeetCode75;
 
 public class Code16_643_Maximum_Average_Subarray_I {
+
+
+
+
+
+    class Solution {
+        public double findMaxAverage(int[] nums, int k) {
+            int sum = 0;
+
+            // Compute the initial window sum
+            for (int i = 0; i < k; i++) {
+                sum += nums[i];
+            }
+
+            int maxSum = sum;
+
+            // Slide the window through the array
+            for (int i = k; i < nums.length; i++) {
+                sum += nums[i] - nums[i - k];  // Slide window
+                maxSum = Math.max(maxSum, sum);
+            }
+
+            return (double) maxSum / k;
+        }
+    }
+
+
 }
