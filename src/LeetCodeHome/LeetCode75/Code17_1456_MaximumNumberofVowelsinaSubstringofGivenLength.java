b704daaae9419f429bacdfdceac9a222e8d4581e
burtohee <burtohe@gmail.com>
2025-07-24 00:34:55 -0400

    init 1456 1004

---
+++ src/LeetCodeHome/LeetCode75/Code17_1456_MaximumNumberofVowelsinaSubstringofGivenLength.java
@@ -0,0 +1,4 @@
+package LeetCodeHome.LeetCode75;
+
+public class Code17_1456_MaximumNumberofVowelsinaSubstringofGivenLength {
+}
---
+++ src/LeetCodeHome/LeetCode75/Code18_1004_Max_Consecutive_Ones_III.java
@@ -0,0 +1,4 @@
+package LeetCodeHome.LeetCode75;
+
+public class Code18_1004_Max_Consecutive_Ones_III {
+}
