7b7d17518412f175dad4f11910033b9c50a6e39f
burtohee <burtohe@gmail.com>
2025-08-11 17:40:27 -0400

    Create Code29_933_NumberofRecent_Calls.java

---
+++ src/LeetCodeHome/LeetCode75/Code29_933_NumberofRecent_Calls.java
@@ -0,0 +1,63 @@
+package LeetCodeHome.LeetCode75;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Code29_933_NumberofRecent_Calls {
+
+
+
+
+
+    class RecentCounter {
+        private static final int[] records = new int[10000];
+        private int start;
+        private int end;
+        public RecentCounter() {
+            start = 0;
+            end = 0;
+        }
+        public int ping(int t) {
+            while (start < end && (t - records[start] > 3000)) {
+                start++;
+            }
+            records[end++] = t;
+            return end - start;
+        }
+    }
+
+
+    class RecentCounter3 {
+
+        private Queue<Integer> queue;
+
+        public RecentCounter() {
+            queue = new LinkedList<>();
+        }
+
+        public int ping(int t) {
+                queue.add(t);
+
+                // Remove pings older than 3000 ms
+//                while (!queue.isEmpty() && queue.peek() < t - 3000) {
+                while (queue.peek() < t - 3000) {
+
+                        queue.poll();
+                }
+
+                return queue.size();
+        }
+    }
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
+
+
+
+
+
+
+}
