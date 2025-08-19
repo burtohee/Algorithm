e400bcd087ae57bdefc9dda480e82724d1429df0
burtohee <burtohe@gmail.com>
2025-08-18 20:15:10 -0400

    Update Code31_2095_Deletethe_Middle_NodeofaLinkedList.java

--- src/LeetCodeHome/LeetCode75/Code31_2095_Deletethe_Middle_NodeofaLinkedList.java
+++ src/LeetCodeHome/LeetCode75/Code31_2095_Deletethe_Middle_NodeofaLinkedList.java
@@ -46,6 +46,50 @@ public class Code31_2095_Deletethe_Middle_NodeofaLinkedList {
         return count;
     }
 
+    public ListNode deleteMiddle(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+        ListNode prev = null, slow = head, fast = head;
+
+        while(fast != null && fast.next != null)
+        {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return new ListNode();
+    }
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        static {
+            for (int i = 0; i < 500; i++) {
+                deleteMiddle(null);
+            }
+        }
+        public static ListNode deleteMiddle(ListNode head) {
+            if(head == null || head.next==null)
+                return null;
+            ListNode slow = head, fast = head,prev=null;
+            while(fast!=null && fast.next!=null){
+                prev=slow;
+                slow=slow.next;
+                fast=fast.next.next;
+            }
+            prev.next=slow.next;
+            return head;
+        }
+    }
+
     class Solution4 {
         public ListNode deleteMiddle(ListNode head) {
             if (head == null || head.next == null) {
