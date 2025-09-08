202946b00e58fb85260a2962a9cc1b1aac56b402
burtohee <burtohe@gmail.com>
2025-09-08 13:33:21 -0400

    Update Code34_2130_MaximumTwinSumofaLinkedList.java

--- src/LeetCodeHome/LeetCode75/Code34_2130_MaximumTwinSumofaLinkedList.java
+++ src/LeetCodeHome/LeetCode75/Code34_2130_MaximumTwinSumofaLinkedList.java
@@ -11,6 +11,31 @@ public class Code34_2130_MaximumTwinSumofaLinkedList {
     }
 
 
+    class Solution {
+        public int pairSum(ListNode head) {
+            int ans = 0;
+            ListNode newList = null;
+            ListNode current = head;
+            ListNode currHalf = head;
+
+            while (currHalf != null && currHalf.next != null) {
+                currHalf = currHalf.next.next;
+                ListNode temp = current.next;
+                current.next = newList;
+                newList = current;
+                current = temp;
+            }
+
+            while (current != null) {
+                ans = Math.max(ans, current.val + newList.val);
+                current = current.next;
+                newList = newList.next;
+            }
+
+            return ans;
+        }
+    }
+
     /**
      * Definition for singly-linked list.
      * public class ListNode {
