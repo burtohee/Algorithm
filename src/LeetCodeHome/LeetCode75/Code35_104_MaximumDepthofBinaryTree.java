528f4d4b490852353a32273d7652c773a780e6fe
burtohee <burtohe@gmail.com>
2025-09-23 17:02:02 -0400

    Create Code35_104_MaximumDepthofBinaryTree.java

---
+++ src/LeetCodeHome/LeetCode75/Code35_104_MaximumDepthofBinaryTree.java
@@ -0,0 +1,57 @@
+package LeetCodeHome.LeetCode75;
+
+import com.sun.source.tree.Tree;
+
+public class Code35_104_MaximumDepthofBinaryTree {
+
+
+     public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution {
+        public int maxDepth(TreeNode root) {
+            if(root == null)
+            {
+                return 0;
+            }
+            int lcount = 0, rcount = 0;
+            if(root.left != null)
+            {
+                lcount = maxDepth(root.left);
+            }
+            if(root.right != null)
+            {
+                rcount = maxDepth(root.right);
+            }
+            return Math.max(lcount, rcount) + 1;
+        }
+    }
+
+
+
+}
